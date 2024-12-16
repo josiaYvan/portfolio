d53e76705931f1a24e2b39170569f58c6a3dbfb2
Josia Yvan <josiayvan@gmail.com>
2024-12-16 21:37:09 +0300

    linked in

--- src/components/layout/LinkedIn.jsx
+++ src/components/layout/LinkedIn.jsx
@@ -1,15 +1,18 @@
 /* eslint-disable react/button-has-type */
 import { LinkedinFilled } from '@ant-design/icons';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function LinkedIn() {
   return (
-    <div className='absolute left-80 -bottom-20'>
-      <button className='flex justify-center items-center w-10 h-10 rounded backdrop-blur-md bg-yellow-900'>
-        <LinkedinFilled className='text-yellow-500' />
-        <div className='absolute top-14 w-[2px] h-16 rounded bg-yellow-500' />
-      </button>
-    </div>
+    <Link to='https://www.linkedin.com/in/josia-yvan/' target='_blank'>
+      <div className='absolute left-80 -bottom-20'>
+        <button className='flex justify-center items-center w-10 h-10 rounded backdrop-blur-md bg-yellow-900'>
+          <LinkedinFilled className='text-yellow-500' />
+          <div className='absolute top-14 w-[2px] h-16 rounded bg-yellow-500' />
+        </button>
+      </div>
+    </Link>
   );
 }
 
