5129d01320bf00eeec6350ea3a87d45530af3251
Josia Yvan <josiayvan@gmail.com>
2024-12-17 09:25:41 +0300

    github link

--- src/components/layout/LinkedIn.jsx
+++ src/components/layout/LinkedIn.jsx
@@ -1,19 +1,24 @@
 /* eslint-disable react/button-has-type */
-import { LinkedinFilled } from '@ant-design/icons';
+import { GithubFilled, LinkedinFilled } from '@ant-design/icons';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { myStyle } from '../../utils/style';
 
 function LinkedIn({ themeIsDark }) {
   return (
-    <Link to='https://www.linkedin.com/in/josia-yvan/' target='_blank'>
-      <div className='absolute left-80 -bottom-20'>
+    <div className='absolute left-80 flex flex-col space-y-2 items-center -bottom-32'>
+      <Link to='https://linkedin.com/in/josia-yvan/' target='_blank'>
         <button className='flex justify-center items-center w-10 h-10 rounded shadow-md backdrop-blur-md' style={{ backgroundColor: themeIsDark ? myStyle.yellowDark : myStyle.white }}>
           <LinkedinFilled className='text-yellow-500' style={{ color: !themeIsDark && myStyle.brown }} />
-          <div className='absolute top-14 w-[2px] h-16 rounded bg-yellow-500' style={{ backgroundColor: !themeIsDark && myStyle.brown }} />
         </button>
-      </div>
-    </Link>
+      </Link>
+      <Link to='https://github.com/josiaYvan/' target='_blank'>
+        <button className='flex justify-center items-center w-10 h-10 rounded shadow-md backdrop-blur-md' style={{ backgroundColor: themeIsDark ? myStyle.yellowDark : myStyle.white }}>
+          <GithubFilled className='text-yellow-500' style={{ color: !themeIsDark && myStyle.brown }} />
+        </button>
+      </Link>
+      <div className='w-[2px] h-16 rounded bg-yellow-500' style={{ backgroundColor: !themeIsDark && myStyle.brown }} />
+    </div>
   );
 }
 
