66f0ad8c9b56d96e33179d184f75d98f51b87184
Josia Yvan <josiayvan@gmail.com>
2024-12-17 20:46:06 +0300

    animation scroll

--- src/components/layout/Menu.jsx
+++ src/components/layout/Menu.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import {
   HomeIcon,
   UserIcon,
@@ -5,52 +6,113 @@ import {
   BriefcaseIcon,
   ChatBubbleBottomCenterTextIcon
 } from '@heroicons/react/16/solid';
+import { motion } from 'framer-motion';
 import React from 'react';
 import { myStyle } from '../../utils/style';
 
 function Menu({ activeSection }) {
+  const handleScroll = (id) => {
+    if (id === 'home') {
+      // Défile vers le haut de la page
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    } else {
+      const section = document.getElementById(id);
+      if (section) {
+        section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
+    }
+  };
+
+  // Framer-motion animation variants
+  const iconVariants = {
+    initial: { scale: 1 },
+    hover: { scale: 1.2, rotate: 10 },
+    tap: { scale: 0.9 }
+  };
+
   return (
     <div className='z-20 fixed bottom-8 left-1/2 transform -translate-x-1/2 w-80'>
-      <div
+      <motion.div
         className='p-4 rounded-full flex justify-around items-center backdrop-blur-md shadow-lg'
         style={{ backgroundColor: myStyle.yellowDark }}
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
       >
-        <a href='#home'>
+        <motion.button
+          onClick={() => handleScroll('home')}
+          variants={iconVariants}
+          whileHover='hover'
+          whileTap='tap'
+          initial='initial'
+          className='cursor-pointer'
+        >
           <HomeIcon
             className={`w-6 h-6 ${
               activeSection === 'home' ? 'text-yellow-500' : 'text-white'
             }`}
           />
-        </a>
-        <a href='#about'>
+        </motion.button>
+        <motion.button
+          onClick={() => handleScroll('about')}
+          variants={iconVariants}
+          whileHover='hover'
+          whileTap='tap'
+          initial='initial'
+          className='cursor-pointer'
+        >
           <UserIcon
             className={`w-6 h-6 ${
               activeSection === 'about' ? 'text-yellow-500' : 'text-white'
             }`}
           />
-        </a>
-        <a href='#skills'>
+        </motion.button>
+
+        <motion.button
+          onClick={() => handleScroll('skills')}
+          variants={iconVariants}
+          whileHover='hover'
+          whileTap='tap'
+          initial='initial'
+          className='cursor-pointer'
+        >
           <BookOpenIcon
             className={`w-6 h-6 ${
               activeSection === 'skills' ? 'text-yellow-500' : 'text-white'
             }`}
           />
-        </a>
-        <a href='#projects'>
+        </motion.button>
+
+        <motion.button
+          onClick={() => handleScroll('projects')}
+          variants={iconVariants}
+          whileHover='hover'
+          whileTap='tap'
+          initial='initial'
+          className='cursor-pointer'
+        >
           <BriefcaseIcon
             className={`w-6 h-6 ${
               activeSection === 'projects' ? 'text-yellow-500' : 'text-white'
             }`}
           />
-        </a>
-        <a href='#contact'>
+        </motion.button>
+
+        <motion.button
+          onClick={() => handleScroll('contact')}
+          variants={iconVariants}
+          whileHover='hover'
+          whileTap='tap'
+          initial='initial'
+          className='cursor-pointer'
+        >
           <ChatBubbleBottomCenterTextIcon
             className={`w-6 h-6 ${
               activeSection === 'contact' ? 'text-yellow-500' : 'text-white'
             }`}
           />
-        </a>
-      </div>
+        </motion.button>
+      </motion.div>
     </div>
   );
 }
