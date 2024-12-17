de4bf9089240db4ce2a965b39bd3ccbb02a26584
Josia Yvan <josiayvan@gmail.com>
2024-12-17 14:16:56 +0300

    clean styles.js

--- src/utils/style.js
+++ src/utils/style.js
@@ -1,9 +1,6 @@
 export const myStyle = {
   bg: '#1F1F1F',
   darkColor: '#e8e8e8',
-  blue: '#4f85f9',
-  darkBlue: '#262F43',
-  accent: '#FFEBBC',
   brown: 'rgb(84, 61, 70)',
   white: '#fff',
   light: 'rgb(254, 251, 236)',
