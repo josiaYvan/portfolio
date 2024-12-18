956e9c42875b41a5ffa73466bc3ce0ec42dcada4
Josia Yvan <josiayvan@gmail.com>
2024-12-18 08:24:16 +0300

    couleur ok

--- src/utils/style.js
+++ src/utils/style.js
@@ -3,7 +3,8 @@ export const myStyle = {
   darkColor: '#e8e8e8',
   brown: 'rgb(84, 61, 70)',
   white: '#fff',
-  light: 'rgb(254, 251, 236)',
+  // light: 'rgb(254, 251, 236)',
+  light: '#F3F3F3',
   yellowDark: '#6754003d',
   yellow: 'rgb(227, 178, 0)',
   path: '#E5E5E5'
