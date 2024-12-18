ac7f152b2e873e5301efccbd6024f8f0e420a583
Josia Yvan <josiayvan@gmail.com>
2024-12-18 21:01:52 +0300

    contact

--- src/components/layout/Contact.jsx
+++ src/components/layout/Contact.jsx
@@ -7,24 +7,32 @@ import { myStyle } from '../../utils/style';
 function Contact({ themeIsDark }) {
   return (
     <section id='contact'>
-      <div className='mt-36'>
+      <div className='mt-16 lg:mt-36'>
+        {/* Titre */}
         <div className='flex flex-col items-center'>
           <p className='text-gray-400 text-xs'>Get in touch</p>
           <h1 className='font-medium text-2xl text-yellow-500'>Contactez Moi</h1>
         </div>
 
-        <div className='my-20 rounded-lg'>
-          <div className='flex flex-col md:flex-row justify-between'>
-
+        {/* Contenu */}
+        <div className='my-10 lg:my-20 rounded-lg px-10'>
+          <div className='flex flex-col md:flex-row justify-between gap-2 lg:gap-6'>
             {/* Section Contact Email et WhatsApp */}
-            <div className='space-y-4'>
+            <div className='flex flex-col items-center space-y-4 md:items-start'>
               {/* Carte Email */}
-              <div className='p-4 rounded-xl shadow-lg w-80 text-center' style={{ backgroundColor: themeIsDark ? myStyle.yellowDark : myStyle.white }}>
+              <div
+                className='p-4 rounded-xl shadow-lg w-80 text-center'
+                style={{ backgroundColor: themeIsDark ? myStyle.yellowDark : myStyle.white }}
+              >
                 <div className='text-2xl'>
                   <MailOutlined />
                 </div>
                 <h2 className='mb-2'>Email</h2>
-                <Link to='mailto:example@mail.com' target='_blank' className='text-yellow-500 flex justify-center text-xs group'>
+                <Link
+                  to='mailto:example@mail.com'
+                  target='_blank'
+                  className='text-yellow-500 flex justify-center text-xs group'
+                >
                   <p>Un mail par ici</p>
                   <span className='ml-1 group-hover:ml-3 transition-all duration-300 ease-in-out'>
                     →
@@ -33,12 +41,19 @@ function Contact({ themeIsDark }) {
               </div>
 
               {/* Carte WhatsApp */}
-              <div className='p-4 rounded-xl shadow-lg w-80 text-center' style={{ backgroundColor: themeIsDark ? myStyle.yellowDark : myStyle.white }}>
+              <div
+                className='p-4 rounded-xl shadow-lg w-80 text-center'
+                style={{ backgroundColor: themeIsDark ? myStyle.yellowDark : myStyle.white }}
+              >
                 <div className='text-2xl'>
                   <WhatsAppOutlined />
                 </div>
                 <h2 className='mb-2'>Whatsapp</h2>
-                <Link to='https://api.whatsapp.com/send?phone=261386183236&text=Hello' target='_blank' className='text-yellow-500 flex justify-center text-xs group'>
+                <Link
+                  to='https://api.whatsapp.com/send?phone=261386183236&text=Hello'
+                  target='_blank'
+                  className='text-yellow-500 flex justify-center text-xs group'
+                >
                   <p>Ou un SMS par là</p>
                   <span className='ml-1 group-hover:ml-3 transition-all duration-300 ease-in-out'>
                     →
@@ -48,42 +63,66 @@ function Contact({ themeIsDark }) {
             </div>
 
             {/* Formulaire de Contact */}
-            <div className='pl-20'>
-              <h2 className='text-xl font-bold mb-4'>Un petit message pour en savoir plus ?</h2>
+            <div className='flex-2 px-4 mx-auto md:mt-0 mt-10'>
+              <h2 className='text-md lg:text-xl font-bold mb-4'>Un petit message pour en savoir plus ?</h2>
 
-              <form>
+              <form className='w-[350px] mx-auto'>
                 {/* Champ Nom */}
                 <div className='mb-4'>
-                  <label htmlFor='name' className='relative top-3 left-5 px-2 text-xs' style={{ backgroundColor: themeIsDark ? myStyle.bg : myStyle.light }}>Nom</label>
+                  <label
+                    htmlFor='name'
+                    className='relative top-3 left-5 px-2 text-xs'
+                    style={{
+                      backgroundColor: themeIsDark ? myStyle.bg : myStyle.light
+                    }}
+                  >
+                    Nom
+                  </label>
                   <input
                     type='text'
                     id='name'
                     placeholder='Votre nom'
-                    className='w-full p-6 rounded-2xl border-2 border-gray-500 text-sm'
+                    className='w-full p-6 rounded-xl border-2 border-gray-500 text-sm'
                     style={{ backgroundColor: themeIsDark ? myStyle.bg : myStyle.light }}
                   />
                 </div>
 
                 {/* Champ Email */}
                 <div className='mb-4'>
-                  <label htmlFor='email' className='relative top-3 left-5 px-2 text-xs' style={{ backgroundColor: themeIsDark ? myStyle.bg : myStyle.light }}>Mail</label>
+                  <label
+                    htmlFor='email'
+                    className='relative top-3 left-5 px-2 text-xs'
+                    style={{
+                      backgroundColor: themeIsDark ? myStyle.bg : myStyle.light
+                    }}
+                  >
+                    Mail
+                  </label>
                   <input
                     type='email'
                     id='email'
                     placeholder='Votre mail'
-                    className='w-full p-6 rounded-2xl border-2 border-gray-500 text-sm'
+                    className='w-full p-6 rounded-xl border-2 border-gray-500 text-sm'
                     style={{ backgroundColor: themeIsDark ? myStyle.bg : myStyle.light }}
                   />
                 </div>
 
                 {/* Champ Message */}
                 <div className='mb-6'>
-                  <label htmlFor='message' className='relative top-3 left-5 px-2 text-xs' style={{ backgroundColor: themeIsDark ? myStyle.bg : myStyle.light }}>Message</label>
+                  <label
+                    htmlFor='message'
+                    className='relative top-3 left-5 px-2 text-xs'
+                    style={{
+                      backgroundColor: themeIsDark ? myStyle.bg : myStyle.light
+                    }}
+                  >
+                    Message
+                  </label>
                   <textarea
                     id='message'
                     rows='4'
                     placeholder='Votre message'
-                    className='w-full p-6 rounded-2xl border-2 border-gray-500 text-sm'
+                    className='w-full p-6 rounded-xl border-2 border-gray-500 text-sm'
                     style={{ backgroundColor: themeIsDark ? myStyle.bg : myStyle.light }}
                   />
                 </div>
@@ -91,8 +130,10 @@ function Contact({ themeIsDark }) {
                 {/* Bouton Envoyer */}
                 <button
                   type='submit'
-                  className='px-4 py-2 bg-yellow-500 rounded-lg hover:bg-yellow-600  transition'
-                  style={{ color: (!themeIsDark ? myStyle.white : myStyle.bg) }}
+                  className='px-4 py-2 bg-yellow-500 rounded-lg hover:bg-yellow-600 transition'
+                  style={{
+                    color: !themeIsDark ? myStyle.white : myStyle.bg
+                  }}
                 >
                   Envoyer
                 </button>
